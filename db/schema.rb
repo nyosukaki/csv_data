db5890c90c6c2cc4d9f9f92ee2bc4a578b28069e
nyosukaki <pikaru1994@gmail.com>
2019-10-30 21:10:44 +0900

    userモデルの作成、マイグレーションの編集・実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 0) do
+=======
+ActiveRecord::Schema.define(version: 2019_10_30_114948) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+>>>>>>> userモデルの作成、マイグレーションファイル編集・実行
 
 end
