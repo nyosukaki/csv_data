dccbd25a3418bae2159110d84a277de0aaef0e1c
nyosukaki <pikaru1994@gmail.com>
2019-10-31 21:49:57 +0900

    import_csv.rbのリファクタリング

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -1,13 +1,28 @@
 class ImportCsv < ApplicationRecord
 
-  def self.import(path)
+  def self.user_data
+     
+    path = File.join Rails.root, "db/csv_data/csv_data.csv"
+
+      list = []
+
     CSV.foreach(path, headers: true) do |row|
-      User.create(
+      list << {
         name: row["name"],
         age: row["age"],
         address: row["address"]
-      )
+      }
     end
+
+    puts "インポート処理を開始"
+
+    begin
+      User.create!(list)
+      puts "インポート完了!!"
+    rescue ActiveModel::UnknownAttributeError => invalid
+      puts "インポートに失敗：UnknownAttributeError"
+    end
+
   end
 
 end
